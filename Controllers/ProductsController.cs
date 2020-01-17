e3d12ac3a3084a917fcc0dae04794311887a70d5
Vinshuka <melissa.ann.christie@gmail.com>
2020-01-16 21:16:45 -0500

    Add project files.

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab2.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public ActionResult Index()
+        {
+            List<String> products = new List<String>{ "Laptop", "Tablet", "Desktop" };
+            ViewData.Add("Products", products);
+            return View();
+        }
+
+        public ActionResult Details(string product)
+        {
+            ViewBag.Message = "You selected product: " + product;
+            return View();
+        }
+    }
+}
